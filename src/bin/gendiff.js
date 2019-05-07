088655de36bf35d9aba2f679d18fe8cdacd574f5
Asltyn <asltyn@rambler.ru>
2019-05-07 11:33:00 +0300

    second commit

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+import * as commander from 'commander';
+
+const command = new commander.Command();
+command.description('Compares two configuration files and show a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
