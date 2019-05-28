485ebf10b69468196edbde8a59a4985b47ea7916
asltyn <asltyn@rambler.ru>
2019-05-29 01:01:11 +0300

    refact

--- src/formatters/defaultFormatter.js
+++ src/formatters/defaultFormatter.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
 
-const stringify = (val, level) => {
-  const depthOfOneLevel = 4;
-  const indent = ' '.repeat(level * depthOfOneLevel);
-  if (typeof val !== 'object') return val;
-  const res = Object.entries(val).map(([key, v]) => `${indent}${key}: ${stringify(v, level + 1)}`).join('\n');
-  return `{\n${res}\n${indent}}`;
+const stringify = (value, initialIndent) => {
+  const extraSpacesOnLevel = 2;
+  const iter = (val, innerLevel) => {
+    if (typeof val !== 'object') return val;
+    const indent = `${initialIndent}${' '.repeat(innerLevel * extraSpacesOnLevel)}`;
+    const res = Object.entries(val).map(([key, v]) => `${indent}${key}: ${iter(v, innerLevel + 1)}`).join('\n');
+    return `{\n${res}\n${indent.slice(0, -extraSpacesOnLevel)}}`;
+  };
+  return iter(value, 0);
 };
 
 const propertyAction = {
@@ -18,11 +21,13 @@ const propertyAction = {
 };
 
 const render = (ast) => {
-  const depthOfOneLevel = 4;
+  const extraSpacesOnLevel = 4;
   const iter = (tree, currLevel) => tree.map((node) => {
-    const indent = ' '.repeat(currLevel * depthOfOneLevel);
     const { type, name, value } = node;
-    return propertyAction[type](name, value, indent, val => stringify(val, currLevel + 1), val => iter(val, currLevel + 1));
+    const indent = ' '.repeat(currLevel * extraSpacesOnLevel);
+    const indentForNextLevel = ' '.repeat((currLevel + 1) * extraSpacesOnLevel);
+    const process = propertyAction[type];
+    return process(name, value, indent, v => stringify(v, indentForNextLevel), v => iter(v, currLevel + 1));
   });
   return _.flattenDeep(iter(ast, 0)).join('\n');
 };
